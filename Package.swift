7e44df1284ea3db09eef104a6b1e85d7007df718
Florian Gabach <florian.gabach@gmail.com>
2021-01-28 10:22:28 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Reusable",
-    platforms: [.iOS(.v8), .tvOS(.v9)],
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         .library(name: "Reusable", targets: ["Reusable"])
     ],
@@ -14,5 +14,5 @@ let package = Package(
         sources: ["Sources"]
       )
     ],
-    swiftLanguageVersions: [.v4, .v5]
+    swiftLanguageVersions: [.v5]
 )
